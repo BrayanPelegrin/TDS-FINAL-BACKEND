abf6872ce017706e4fa7bfdd5956d27be4018404
Miguel Cruz <miguelangelcruz200217@gmail.com>
2022-10-30 19:11:49 -0400

    UPDATE JWT

---
+++ TecnoStore.Core/Services/UsuarioServices.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TecnoStore.Core.Common;
+using TecnoStore.Core.DTOs;
+using TecnoStore.Core.Interfaces;
+
+namespace TecnoStore.Core.Services
+{
+    public class UsuarioServices : ILogin<LoginDTO>
+    {
+        // LISTA TEMPORAL DE USUARIOS.
+        List<UsuarioDTO> user = new List<UsuarioDTO>();
+
+        private readonly AppSettings _appSettings;
+
+        // CONSTRUCTOR PARA OBTENER EL TOKEN.
+        public UsuarioServices(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        void Insertar()
+        {
+
+            user.Add(new UsuarioDTO()
+            {
+                ID = 1,
+                Name = "Miguel",
+                Correo = "miguelangelcruz200217@gmail.com",
+                Password = "miguel2002"
+            });
+
+        }
+
+        public LoginDTO ValidarSesion(LoginDTO Credenciales)
+        {
+            // LLAMANDO EL METODO PARA INSERTAR DATOS EN LA LISTA.
+            Insertar();
+
+            var usuario = user.Where(d => d.Correo == Credenciales.Correo 
+                          && d.Password == Credenciales.Password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return null;
+            }
+
+            Credenciales.Token = getToken(usuario);
+
+            // RETORNANDO LAS CREDENCIALES JUNTO AL TOKEN.
+            return Credenciales;
+        }
+
+        private string getToken(UsuarioDTO user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_appSettings.Token);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.ID.ToString()),
+                        new Claim(ClaimTypes.Email, user.Correo)
+                    }
+
+                    ),
+
+                // EXPIRACION DEL TOKEN.
+                Expires = DateTime.UtcNow.AddDays(60),
+                
+                // ENCRIPTAR INFORMACION.
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), 
+                SecurityAlgorithms.HmacSha256Signature),
+
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+
+
+        }
+
+    }
+}
--- TecnoStore/Program.cs
+++ TecnoStore/Program.cs
@@ -1,22 +1,66 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
-using TecnoStore.Infrastructure;
+using System.Text;
+using TecnoStore.Core.Common;
+using TecnoStore.Core.DTOs;
+using TecnoStore.Core.Interfaces;
+using TecnoStore.Core.Services;
 using TecnoStore.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Servicios(builder.Configuration);
+builder.Services.AddDbContext<TecnoStoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSQL"));
+});
 
 // Add services to the container.
 
 builder.Services.AddControllers().AddNewtonsoftJson(options => 
    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
 );
+
+// OBTENER TOKEN DESDE APP-SETTINGS
+
+var appSettingsSection = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettingsSection);
+
+// JWT 
+
+var appSettings = appSettingsSection.Get<AppSettings>();
+var key = Encoding.ASCII.GetBytes(appSettings.Token);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.SaveToken = true;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<ILogin<LoginDTO>, UsuarioServices>();
+
 var app = builder.Build();
+
+app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
